bf2b6528ebbb7370affecda2629724396e178061
Thijs <Thijsjuh97@hotmail.com>
2023-10-09 00:11:53 +0200

    Refined 1 resource, good/bad weather tests

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,13 +3,20 @@ import escapeHtml from "escape-html";
 import {users} from "../models/db.js";
 import bcrypt from "bcrypt";
 import auctionController from "./auctionController.js";
+import jwt from "jsonwebtoken";
 const authController = {
 async login(req,res){
     try{
+
         const {username, password} = req.body
         const hash = await authModel.hashPassword(password);
         const user = users.find((user) => user.username === username)
         if (user || await user.comparePassword(password, hash)){
+            const MY_SECRET = "hello";
+            const token = jwt.sign(user,MY_SECRET, { expiresIn: "1h"})
+            res.cookie("token", token, {
+                httpOnly: true
+            });
             return res.status(200).json({message:"Logging in...."})
         } else {
             return res.status(401).json({message: "Incorrect username or password, try again!"})
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authController from "../controllers/authController.js";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/login",authController.login);
